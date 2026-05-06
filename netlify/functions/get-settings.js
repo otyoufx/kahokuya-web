8115f500b08ab530d5308bd9f8c74f8ee35bc8ef
otyoufx <otyoufx@gmail.com>
2026-05-07 02:31:05 +0900

    デバッグログ追加

--- netlify/functions/get-settings.js
+++ netlify/functions/get-settings.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 exports.handler = async () => {
+console.log("DEBUG: get-settings START");
   try {
     const store = blobs().store("settings");
 
