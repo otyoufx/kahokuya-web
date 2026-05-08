166265075516d9c25a7fbb14b5944add1544e2bc
otyoufx <otyoufx@gmail.com>
2026-05-08 16:58:32 +0900

    nodfetch delete

--- netlify/functions/get-settings.js
+++ netlify/functions/get-settings.js
@@ -1,5 +1,3 @@
-const fetch = require("node-fetch");
-
 exports.handler = async () => {
   console.log("DEBUG: get-settings START");
 
--- netlify/functions/save-settings.js
+++ netlify/functions/save-settings.js
@@ -1,5 +1,3 @@
-const fetch = require("node-fetch");
-
 exports.handler = async (event) => {
   console.log("DEBUG: save-settings START");
 
